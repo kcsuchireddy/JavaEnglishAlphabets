45fe7269170c086e958a2978cf584284e4c89304
Ekanath Reddy <kcsuchireddy>
2019-03-06 16:56:59 +0000

    first commit

---
+++ src/love/programming/PrintAlphabets.java
@@ -0,0 +1,11 @@
+package love.programming;
+
+public class PrintAlphabets {
+
+    public static void main(String[] args) {
+
+        char ch;
+        for (ch = 'A'; ch <= 'Z'; ch++)
+            System.out.println(ch);
+    }
+}
